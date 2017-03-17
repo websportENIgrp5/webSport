7a66a696830415bcc4d0ae44c339edf01d335cb1
mmarin <maxime.marin2015@campus-eni.fr>
2017-03-17 12:19:15 +0100

    Ajout de la carte

---
+++ TP - WebSport - Part20/BLL/MgtPoiCategory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+
+namespace BLL
+{
+    public class MgtPoiCategory
+    {
+        private List<PoiCategory> _category;
+
+        public List<PoiCategory> GetCategory {
+            get
+            {
+                List<PoiCategory> catePoi = new List<PoiCategory>()
+                {
+                    new PoiCategory(1,"depart"),
+                    new PoiCategory(2,"ravito"),
+                    new PoiCategory(3,"arrivee"),
+                    new PoiCategory(4,"checkPoint"),
+                };
+                return catePoi;
+            }
+          }
+
+
+        public MgtPoiCategory()
+        {
+            _category = GetCategory;
+        }
+
+        public int GetIdByName(string name)
+        {
+            foreach(PoiCategory catePoi in _category)
+            {
+                if(catePoi.Name.Equals(name))
+                {
+                    return catePoi.Id;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
--- TP - WebSport - Part20/BO/PoiCategory.cs
+++ TP - WebSport - Part20/BO/PoiCategory.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    
-    class PoiCategory
+    public class PoiCategory
     {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public PoiCategory()
+        {
+
+        }
+
+        public PoiCategory(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
