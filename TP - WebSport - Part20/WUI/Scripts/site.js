c2d775dd4f8d49f4510662a93fe23ac90ca00460
ajudic <test@test.fr>
2017-03-17 09:23:45 +0100

    Dashboard

---
+++ TP - WebSport - Part20/WUI/Scripts/site.js
@@ -0,0 +1,8 @@
+﻿// Account menu
+var url = window.location.pathname;
+var url_parts = url.replace(/\/\s*$/, '').split('/');
+
+if ($('#' + url_parts[3])) {
+    $('.account-nav-item').removeClass('active');
+    $('#' + url_parts[3]).addClass('active');
+}
